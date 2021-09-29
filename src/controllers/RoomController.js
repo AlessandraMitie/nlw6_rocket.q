1dd6f5b33f4e5384985fc9450b331408bd68d538
AlessandraMitie <alessandra.mitie@gmail.com>
2021-09-28 21:58:20 -0300

    excluir e marcar pergunta como lida

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -6,23 +6,44 @@ module.exports = {
         const db = await Database()
         const pass = req.body.password
         let roomId
+        let isRoom = true
 
-        for(var i=0; i < 6; i++){
-            i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-            roomId += Math.floor(Math.random() * 10).toString()
+        while(isRoom){
+            // gera o número da sala
+            for(var i=0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+            }
+
+            // verificar se o número já existe
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+            //some = se a ccondição existir (o primeiro que encontrar), vai retornar true
+        
+            if(!isRoom){
+                //inserir a sala no banco de dados
+                await db.run(`INSERT INTO rooms (
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
         }
-
-        //inserir no banco de dados
-        await db.run(`INSERT INTO rooms (
-            id,
-            pass
-        ) VALUES (
-            ${parseInt(roomId)},
-            ${pass}
-        )`)
 
         await db.close()
 
         res.redirect(`/room/${roomId}`)
+    },
+
+    async open(req, res){
+        const db = await Database()
+        const roomId = req.params.room
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+        const questionsRead = await db.all(`SELECT *FROM questions WHERE room = ${roomId} and read = 1`)
+
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
     }
 }
